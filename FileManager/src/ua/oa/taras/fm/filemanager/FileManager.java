2e7b6c015f9762783c722f3ddb49feedec23a8cc
Yeti <shevchenko_tg@ukr.net>
2015-05-25 21:49:20 +0300

    run view

--- FileManager/src/ua/oa/taras/fm/filemanager/FileManager.java
+++ FileManager/src/ua/oa/taras/fm/filemanager/FileManager.java
@@ -1,13 +1,22 @@
 package ua.oa.taras.fm.filemanager;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class FileManager extends Application {
 
 	@Override
-	public void start(Stage primaryStage) {
-		
+	public void start(Stage primaryStage) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("/ua/oa/taras/fm/view/FmView.fxml"));
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("SimpleFileManager");
+		primaryStage.show();
 	}
 
 	public static void main(String[] args) {
